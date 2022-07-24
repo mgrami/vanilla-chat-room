96e8e128730243bb0eff8d74a22af3164df49541
mgrami <m.grami.m@gmail.com>
2022-07-24 07:42:02 -0700

    process.env.PORT

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomName: req.params.room })
 })
 
-server.listen(3000)
+server.listen(process.env.PORT || 3000)
 
 io.on('connection', socket => {
   socket.on('new-user', (room, name) => {
